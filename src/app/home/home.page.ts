8b0d7f63a8e32140b0d022678b879684df4f5154
mcwilliamska <mcwilliamska2016@gmail.com>
2021-05-11 20:21:32 -0400

    Initial commit with pages, various UI elements, navigation, and initial sliders

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // private database: SQLiteObject;
+
+  constructor() {
+    // private sqlite: SQLite
+    // this.sqlite.create({
+    //   name: 'data.db',
+    //   location: 'default'
+    // })
+    //   .then((db: SQLiteObject) => {
+    //     db.executeSql('create table danceMoves(name VARCHAR(32))', [])
+    //       .then(() => console.log('Executed SQL'))
+    //       .catch(e => console.log(e));
+    //   })
+    //   .catch(e => console.log(e));
+  }
+
+
 
 }
